c5c259177b1ef9eb4042ab69c58307fc24e6621d
Hung <hung16132016@gmail.com>
2024-05-13 19:27:53 +0700

    feat: brand popular

--- src/main/java/com/kiotfpt/controller/ProductController.java
+++ src/main/java/com/kiotfpt/controller/ProductController.java
@@ -22,49 +22,49 @@ import com.kiotfpt.service.ProductService;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/product")
 public class ProductController {
 
 	@Autowired
 	private ProductService service;
 
 //	api line 13
-	@GetMapping("/product/get-all")
+	@GetMapping("/get-all")
 	public ResponseEntity<ResponseObject> getAllProduct(@RequestParam(name = "page") Integer page, @RequestParam(name = "amount") Integer amount) {
 		return service.getAllProduct(page, amount);
 	}
 
 //	api line 18
-	@GetMapping("/product/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<ResponseObject> getProductById(@PathVariable int id) {
 		return service.getProductById(id);
 	}
 	
 //	api line 11
-	@GetMapping("/product/new")
+	@GetMapping("/new")
 	public ResponseEntity<ResponseObject> getNew8Added() {
 		return service.getNew8Added();
 	}
 	
 //	api line 14
-	@GetMapping("/product/get-by-category")
+	@GetMapping("/get-by-category")
 	public ResponseEntity<ResponseObject> getByCategoryID(@RequestParam(name = "categoryID") Integer category_id, @RequestParam(name = "page") Integer page, @RequestParam(name = "amount") Integer amount) {
 		return service.getByCategoryID(category_id, page, amount);
 	}
 	
 //	api line 7
-	@GetMapping("/product/search")
+	@GetMapping("/search")
 	public ResponseEntity<ResponseObject> getByKeyword(@RequestParam(name = "key") String keyword, @RequestParam(name = "page") Integer page, @RequestParam(name = "amount") Integer amount) {
 		return service.getByKeyword(keyword, page, amount);
 	}
 
-	@GetMapping("/product/get-by-shop")
+	@GetMapping("/get-by-shop")
 	public ResponseEntity<ResponseObject> getProductByShop(@RequestParam(name = "shop_id") Integer shopID,
 			@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int amount) {
 		return service.findByShopId(shopID, page, amount);
 	}
 
-	@PostMapping("/product/create")
+	@PostMapping("/create")
 	public ResponseEntity<ResponseObject> createProduct(@RequestBody ProductRequest product) throws IOException {
 		return service.createProduct(product);
 	}
@@ -74,50 +74,52 @@ public class ProductController {
 		return service.deleteProduct(id);
 	}
 
-	@PutMapping("/product/update/{id}")
+	@PutMapping("/update/{id}")
 	public ResponseEntity<ResponseObject> updateProduct(@PathVariable int id, @RequestBody ProductRequest obj) {
 		return service.updateProduct(id, obj);
 	}
 	
-	@GetMapping("/product/discount")
+	@GetMapping("/discount")
 	public ResponseEntity<ResponseObject> getDiscountedProducts() {
 		return service.getDiscountedProducts();
 	}
 
-	@GetMapping("/product/official")
+	@GetMapping("/official")
 	public ResponseEntity<ResponseObject> getOfficialProducts() {
 		return service.getOfficialProducts();
-	@GetMapping("/product/top-deal")
+	}
+	
+	@GetMapping("/top-deal")
 	public ResponseEntity<ResponseObject> getTopDealProduct() {
 		return service.getTopDealProduct();
 	}
 
-//	@GetMapping("/product/search")
+//	@GetMapping("/search")
 //	public List<Product> searchByName(@RequestParam(name = "query", required = true) String name) {
 //		return service.searchByName(name);
 //	}
 
-//	@GetMapping("/product/get-list-popular")
+//	@GetMapping("/get-list-popular")
 //	public ResponseEntity<ResponseObject> getProductPopular() {
 //		return service.getProductPopular();
 //	}
 //	
-//	@GetMapping("/product/get-list-product")
+//	@GetMapping("/get-list-product")
 //	public ResponseEntity<ResponseObject> getProductByPage(@RequestParam("page") int page) {
 //		return service.getProductByPage(page);
 //	}
 //	
-//	@GetMapping("/product")
+//	@GetMapping("")
 //	public ResponseEntity<ResponseObject> getProductByID(@RequestParam("id") int product_id) {
 //		return service.getProductByID(product_id);
 //	}
 //	
-//	@GetMapping("/product/filter")
+//	@GetMapping("/filter")
 //	public ResponseEntity<ResponseObject> getProductByID(@RequestParam("price") String price) {
 //		return service.getSortedProducts(price);
 //	}
 //	
-//	@GetMapping("/product/search")
+//	@GetMapping("/search")
 //	public ResponseEntity<ResponseObject> getProductByName(@RequestParam("name") String name) {
 //		return service.getProductByName(name);
 //	}
--- src/main/java/com/kiotfpt/service/BrandService.java
+++ src/main/java/com/kiotfpt/service/BrandService.java
@@ -3,8 +3,10 @@ package com.kiotfpt.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import com.kiotfpt.model.Brand;
 import com.kiotfpt.model.ResponseObject;
 import com.kiotfpt.repository.BrandRepository;
 import com.kiotfpt.repository.ProductRepository;
+import com.kiotfpt.response.BrandResponse;
 
 @Service
 public class BrandService {
@@ -39,4 +42,22 @@ public class BrandService {
 				: ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseObject(false,
 						HttpStatus.NOT_FOUND.toString().split(" ")[0], "Data has not found", new int[0]));
 	}
+	
+    public ResponseEntity<ResponseObject> getPopularBrands() {
+        List<Brand> popularBrands = repository.findPopularBrandsWithLimit(PageRequest.of(0, 4));
+
+        if (popularBrands.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseObject(false, HttpStatus.NOT_FOUND.toString().split(" ")[0],
+                            "No popular brands found.", null));
+        }
+
+        List<BrandResponse> brandResponses = popularBrands.stream()
+                .map(brand -> new BrandResponse(brand, brand.getProducts()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject(true, HttpStatus.OK.toString().split(" ")[0],
+                        "Popular brands found.", brandResponses));
+    }
 }
