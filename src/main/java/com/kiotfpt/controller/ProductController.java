559ec208b42a68b047e1976635bb32159ab0b4fd
Hung <hung16132016@gmail.com>
2024-05-13 18:17:00 +0700

    fix: productcontroller

--- src/main/java/com/kiotfpt/controller/ProductController.java
+++ src/main/java/com/kiotfpt/controller/ProductController.java
@@ -87,6 +87,8 @@ public class ProductController {
 	@GetMapping("/product/official")
 	public ResponseEntity<ResponseObject> getOfficialProducts() {
 		return service.getOfficialProducts();
+	}
+	
 	@GetMapping("/product/top-deal")
 	public ResponseEntity<ResponseObject> getTopDealProduct() {
 		return service.getTopDealProduct();
