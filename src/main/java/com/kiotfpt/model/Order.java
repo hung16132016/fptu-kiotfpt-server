0afaec60e17a9ad57b4c79a80afa76824ab52730
Hung <hung16132016@gmail.com>
2024-08-08 11:19:36 +0700

    fix: remove auto increase id of order

--- src/main/java/com/kiotfpt/model/Order.java
+++ src/main/java/com/kiotfpt/model/Order.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -28,7 +26,6 @@ import lombok.NoArgsConstructor;
 public class Order {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "order_id")
 	private int id;
 
